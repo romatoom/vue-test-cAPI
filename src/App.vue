73e5d391db93009ac7da3c0031d739ea83ac39cf
romatoom <romani@epayservices.com>
2021-10-08 12:15:50 +0500

    delete not used

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,33 @@
 <template>
   <div class="container">
-    <form class="card" @submit.prevent="submit">
+    <form class="card">
       <h1>Auth</h1>
 
       <h3 v-if="error">{{error}}</h3>
 
-      <div class="form-control" :class="{invalid: !form.email.valid && form.email.touched}">
+      <div class="form-control" :class="{invalid: !form.email.valid }">
         <label for="email">Email</label>
-        <input type="email" id="email" v-model="form.email.value" @blur="form.email.blur">
-        <small v-if="form.email.touched && form.email.errors.required">Email field is required</small>
+        <input type="email" id="email" v-model="form.email.value" >
+        <small v-if="form.email.errors.required">Email field is required</small>
       </div>
 
-      <div class="form-control" :class="{invalid: !form.password.valid && form.password.touched}">
+      <div class="form-control" :class="{invalid: !form.password.valid}">
         <label for="password">Password</label>
-        <input type="password" id="password" v-model="form.password.value" @blur="form.password.blur">
-        <small v-if="form.password.touched && form.password.errors.required">Password field is required</small>
-        <small v-else-if="form.password.touched && form.password.errors.minLength">
+        <input type="password" id="password" v-model="form.password.value">
+        <small v-if="form.password.errors.required">Password field is required</small>
+        <small v-else-if="form.password.errors.minLength">
           Password length can't be less then 8. Now it is {{form.password.value.length}}.
         </small>
       </div>
 
       <button class="btn primary" type="submit" :disabled="!form.valid">Submit</button>
     </form>
-
-    <Suspense v-if="submitted">
-      <UsersList />
-      <template #fallback>
-        <div class="loader"></div>
-      </template>
-    </Suspense>
   </div>
 
 </template>
 
 <script>
 import {ref, onErrorCaptured} from 'vue'
-import UsersList from './components/UsersList'
 import {useForm} from './use/form'
 
 const required = val => !!val
@@ -43,7 +35,6 @@ const minLength = num => val => val.length >= num
 
 export default {
   setup() {
-    const submitted = ref(false)
     const error = ref()
     const form = useForm({
       email: {
@@ -56,19 +47,12 @@ export default {
       }
     })
 
-    function submit() {
-      console.log('Email:', form.email.value)
-      console.log('Password:', form.password.value)
-      submitted.value = true
-    }
-
     onErrorCaptured(e => {
       error.value = e.message
     })
 
-    return {form, submit, submitted, error}
+    return {form, error}
   },
-  components: {UsersList}
 }
 </script>
 
